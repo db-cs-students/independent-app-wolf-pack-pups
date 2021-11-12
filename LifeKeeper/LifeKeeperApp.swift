523e2676557ba506360af05bdfe5a6a825e466a7
Iyana G <iyanaamir2004@gmail.com>
2021-11-12 12:04:42 -0500

    Initial commit

---
+++ LifeKeeper/LifeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeKeeperApp.swift
+//  LifeKeeper
+//
+//  Created by Iyana G on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LifeKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
