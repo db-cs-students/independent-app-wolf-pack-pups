0bd7266bced3956dd2bb7fe0968196e9d65ee3d2
Hannah F <hnfannon@gmail.com>
2021-11-15 12:57:31 -0500

    Created background and text of the layout of the screen.

--- LifeKeeper/ContentView.swift
+++ LifeKeeper/ContentView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+            Text("Hello, world!")
+                .padding()
+        }
+                
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ LifeKeeper/ToDoDetails.swift
@@ -0,0 +1,58 @@
+//
+//  ToDoDetails.swift
+//  LifeKeeper
+//
+//  Created by Hannah F on 11/15/21.
+//
+
+import SwiftUI
+
+struct ToDoDetails: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Color("Background")
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 20.0) {
+                HStack(alignment: .top, spacing: 180.0) {
+                    Text("I'm Done")
+                        .font(.system(size: 23.0))
+                    Text("Back")
+                        .font(.system(size: 23.0))
+
+                }
+
+                Text("Finish Project")
+                    .font(.system(size: 40.0))
+                    .padding()
+                
+                Text("History")
+                    .font(.system(size: 20.0))
+                    .foregroundColor(Color.gray)
+                
+                VStack(alignment: .leading, spacing: 30.0) {
+                    Text("Room: W287")
+                        .font(.system(size: 25.0))
+                    Text("Date: 11/5/2021")
+                        .font(.system(size: 25.0))
+                    Text("Time: 2PM")
+                        .font(.system(size: 25.0))
+                    Text("Guests: Bob, Sue, Karen")
+                        .font(.system(size: 25.0))
+                    Text("Notes: Complete by 2PM and make sure it's submitted in MLA formatting.")
+                        .font(.system(size: 25.0))
+                }
+            
+            }
+            
+                
+        }
+        
+    }
+}
+
+struct ToDoDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoDetails()
+    }
+}
