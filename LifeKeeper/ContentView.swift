3611b0aa0b4b78a9276184dc5b55a4924349ac1c
Iyana G <iyanaamir2004@gmail.com>
2021-11-15 12:46:15 -0500

    background gradient.

--- LifeKeeper/ContentView.swift
+++ LifeKeeper/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CustomGradient {
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+//.background(
+//LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .top, endPoint: .bottom)
+//)
+
+struct CustomGradient<Content: View>: View {
+    @ViewBuilder var content: Content
+    
+    var colors: [Color] = [
+        Color("Isabelline"),
+        Color("Linen"),
+        Color("PalePink"),
+        Color("MimiPink"),
+        Color("MintCream"),
+        Color("PowderBlue"),
+        Color("Isabelline"),
+        Color("LavendarWeb"),
+        Color("PeriwinkleCrayola")
+    ]
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: colors), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            content
+        }
     }
 }
