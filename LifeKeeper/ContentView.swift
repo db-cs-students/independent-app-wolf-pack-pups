8921f02940407921d566c9a3e0f02050c97e6a8d
Iyana G <iyanaamir2004@gmail.com>
2021-11-16 12:29:02 -0500

    category tabs

--- LifeKeeper/ContentView.swift
+++ LifeKeeper/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            Text("All")
-                .padding()
-            Text("History")
-            Text("Chemistry")
-            Text("Work")
-        }
+					ZStack {
+						Rectangle()
+							.frame(width: 50, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+							.cornerRadius(25)
+						Text("All")
+							.padding()
+							.foregroundColor(.white)
+						
+					}
+					ZStack {
+						Rectangle()
+							.frame(width: 73, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+							.cornerRadius(25)
+						Text("History")
+							.padding()
+							.foregroundColor(.white)
+					}
+					ZStack {
+						Rectangle()
+							.frame(width: 95, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+							.cornerRadius(25)
+						Text("Chemistry")
+							.padding()
+							.foregroundColor(.white)
+					}
+					ZStack {
+						Rectangle()
+							.frame(width: 57, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+							.cornerRadius(25)
+						Text("Work")
+							.padding()
+							.foregroundColor(.white)
+					}
+					}
     }
 }
 
