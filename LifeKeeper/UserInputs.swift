632b2650d2362703409e08ab11918bc0a3740020
Bladen H <bladenray1@gmail.com>
2021-12-08 13:06:51 -0500

    Add model data into categories and add date filter

--- LifeKeeper/UserInputs.swift
+++ LifeKeeper/UserInputs.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 
-struct Todo: Hashable, Codable {
+struct Todo: Identifiable, Hashable, Codable {
+    var id: UUID = UUID()
     var taskName: String
     var roomNumber: Int
     var categorySelected: String
@@ -19,5 +20,7 @@ struct Todo: Hashable, Codable {
 
 final class ToDos: ObservableObject {
     @Published var todoList: [Todo] = [
-        Todo(taskName: "Do Homework", roomNumber: 304, categorySelected: "Chemistry", date: Date(), time: 1234, guestsInvited: "Karen, Sue, Bob", notes: "Complete early for extra credit")]
+        Todo(taskName: "Do Homework", roomNumber: 304, categorySelected: "Chemistry", date: Date(), time: 1234, guestsInvited: "Karen, Sue, Bob", notes: "Complete early for extra credit"),
+        Todo(taskName: "Do Homework", roomNumber: 304, categorySelected: "Chemistry", date: Date().addingTimeInterval(86400), time: 1234, guestsInvited: "Karen, Sue, Bob", notes: "Complete early for extra credit")
+    ]
 }
