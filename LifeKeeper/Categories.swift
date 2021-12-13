fe1043066bfc9a8443d5e2903419e8b1faa81038
Bladen H <bladenray1@gmail.com>
2021-12-13 12:04:50 -0500

    Merge category button border into backup global variables

--- LifeKeeper/Categories.swift
+++ LifeKeeper/Categories.swift
@@ -109,12 +109,22 @@ struct Categories_Previews: PreviewProvider {
 struct TaskView: View {
     let assignment: String
     let date: Date
-    
+    var borderColor: Color {
+        return [Color("paleYellow"), Color("Loblolly"), Color("PaleOrange")].randomElement()!
+    }
+
     var body: some View {
         ZStack (alignment: .leading) {
             Capsule()
                 .fill(Color("Transgray"))
+
                 .frame(width: 351, height: 60, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke((borderColor),
+                                lineWidth: 3)
+                )
+
             VStack (alignment: .leading) {
                 Text(assignment)
                     .font(.title2)
